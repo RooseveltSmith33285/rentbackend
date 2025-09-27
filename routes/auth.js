30bd216f77c03676e2d95338b4e64406cef32352
DawrAli33285 <lemightyeagle@gmail.com>
2025-09-27 12:20:53 -0700

    new

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 
-const {register,login,getUser}=require('../controller/auth')
+const {register,login,getUser,resetPassword}=require('../controller/auth')
 const router=require('express').Router()
 const {Auth}=require('../middleware/auth')
 
 router.post('/register',register)
 router.post('/login',login)
 router.get('/getUser',Auth,getUser)
-
+router.patch('/resetPassword',resetPassword)
 module.exports=router;
