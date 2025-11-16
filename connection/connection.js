881bace192b0360ba36cf534ee79db8f820ec1c9
DawrAli33285 <lemightyeagle@gmail.com>
2025-11-17 02:48:20 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 
-let connection=mongoose.connect('mongodb://127.0.0.1/newrent')
+// let connection=mongoose.connect('mongodb://127.0.0.1/newrent')
 
 
-// let connection=mongoose.connect('mongodb+srv://developer:developer@cluster0.k1ekxcf.mongodb.net/')
+let connection=mongoose.connect('mongodb+srv://developer:developer@cluster0.k1ekxcf.mongodb.net/')
 
 // const connection=mongoose.connect(`mongodb+srv://user:user@cluster0.pfn059x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 
